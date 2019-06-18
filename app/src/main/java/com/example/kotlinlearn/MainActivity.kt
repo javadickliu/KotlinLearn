1d153635e582a7559c1c4f8b2d2ec4f118f9a995
dongxunliu <531509569@qq.com>
2019-06-18 18:20:12 +0800

    保存

--- app/src/main/java/com/example/kotlinlearn/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearn/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,39 +19,58 @@ class MainActivity : AppCompatActivity() {
 //    var buttn2 :Button= Button(this)
 //    var double = 0.4
 //    val outVar : Int=4
-    var string: String?=null  //todo Kotlin成员变量初始化的时候
+    val string: String? = null  //todo Kotlin成员变量初始化的时候
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-   //     toast(string[0] + "")
-        if(string!=null) {
-            toast(string?.length+"")
-        }
+        //     toast(string[0] + "")
+        // if(string!=null) {
+        //    string?.length
+        //  }
+        //  val stringTemp:Int=if (string!=null) string.length else -1
+        //  val stringTemp1:Int=string!!.length
 
-       //==Kotlin类型检查
-        typeCheck(5)
-
+        //==Kotlin类型检查
+        //  typeCheck(5)
 
 
         //==Kotlin匿名内部类====
-        mainactivity_btn.setOnClickListener(object :View.OnClickListener{
-            override fun onClick(p0: View?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
-        mainactivity_btn.setOnClickListener {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+//        mainactivity_btn.setOnClickListener(object :View.OnClickListener{
+//            override fun onClick(p0: View?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//        })
+//        mainactivity_btn.setOnClickListener {
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        }
 
 
         //====类型转换as关键字
-        typeConvert(6)
+        //   typeConvert(6)
 
 
-        //==
+        //==伴生类调用==
+        // TestKotlinClass.testBoolean
+
+        //=====遍历for==
+       // forTest()
 
 
+//
+//        val intValue:Int=4
+//        val intValue:int=4
+
+        //==Kotlin数字==
+//        val shijinzhi = 11
+//        val shiliujinzhi = 0x0f
+//        val erjinzhi = 0b0010
+//        val doubleValue = 0.4
+//        val floatValue = 0.4f
+//        val largeValue = 100_0000_1000
+        // largeValue.to
+
+        whenUse(1)
     }
 
 
@@ -70,16 +90,16 @@ class MainActivity : AppCompatActivity() {
     /**
      * Kotlin类型检查
      */
-    fun typeCheck(x :Any){
-         if(x is String){//Kotlin智能转换情形一
-             x.substring(0)
-         }
+    fun typeCheck(x: Any) {
+        if (x is String) {//Kotlin智能转换情形一
+            x.substring(0)
+        }
 
-        if(x !is String)//Kotlin智能转换情形二
+        if (x !is String)//Kotlin智能转换情形二
             return
         x.substring(0)
 
-        if(x is String && x.substring(0).equals("")){//Kotlin智能转换情形三
+        if (x is String && x.substring(0).equals("")) {//Kotlin智能转换情形三
 
         }
     }
@@ -88,9 +108,70 @@ class MainActivity : AppCompatActivity() {
     /**
      * Kotlin类型转换
      */
-    fun typeConvert(x :Any){
+    fun typeConvert(x: Any) {
 //        val y:String=x as String//不安全的类型转换
 //        val y:String?=x as? String//安全的类型转换
+    }
+
+    /**
+     * kotlin遍历循环
+     */
+    fun forTest() {
+        //==Kotlin遍历数组==
+        val arrayTest = arrayOf("dongxun1", "dongpeng", "dongin")
+        for (index in arrayTest) {//数组index表示的是索引
+            Log.d(TAG, "" + index)
+        }
+
+        //===Kotlin遍历list集合
+
+        //===Kotlin遍历map集合
+
+        //===Kotlin遍历set集合
+
+    }
+
+    /**
+     * when关键字的使用
+     */
+    fun whenUse(int: Int) {
+
+        when (int) {
+            in 1..10 -> {
+                Log.d(TAG, "int==1..10")
+            }
+            1 -> {
+                Log.d(TAG, "int==1!!!")
+            }
+            11, 12 -> {
+                Log.d(TAG, "int==11,12")
+            }
+            13 -> {
+                Log.d(TAG, "int==13")
+            }
+            is Int -> {
+                Log.d(TAG, "int is int")
+            }
+            else -> {
+                Log.d(TAG, "int==else")
+            }
+        }
+        when {
+            true -> {
+                Log.d(TAG, "true")
+            }
+            false -> {
+                Log.d(TAG, "false")
+            }
+            true -> {
+                Log.d(TAG, "true11111")
+            }
+        }
+    }
+
+
+    companion object {
+        private val TAG = "MainActivity"
     }
 }
 
--- app/src/main/java/com/example/kotlinlearn/TestKotlinClass.kt
+++ app/src/main/java/com/example/kotlinlearn/TestKotlinClass.kt
@@ -1,14 +1,21 @@
 package com.example.kotlinlearn
 
-fun topMethod(){
+fun topMethod() {
 
 }
-internal val topMember:Int=1
-interface TopInterface{
+
+internal val topMember: Int = 1
+
+interface TopInterface {
 
 }
+
 class TestKotlinClass {
 
-    val innerMember:String="test string"
+    val innerMember: String = "test string"
+
+    companion object Obj {
+        var testBoolean = true
+    }
 
 }
